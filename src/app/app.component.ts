70b235f8b8bdfd5180e11084ae772b6cccf27b29
Shawn Huang <shawn@otter.ai>
2020-03-12 02:05:58 -0700

    0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'otter-ai';
+  title = {
+    text: "angular-universal-v9",
+    link: "https://otter.ai",
+    color: "#f00"
+  };
+
+  onClick(event: MouseEvent) {
+    console.log((event.target as any)._data);
+  }
 }
